42024ba480adfb97d78abfe706654596f4791dc9
oksanarutska <42785757+oksanarutska@users.noreply.github.com>
2019-11-24 01:00:40 +0200

    fixed background card

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
     var card = {
         init: function () {
             this.initVar();
-            this.bindEvent()
+            this.bindEvent();
+            
         },
         initVar: function () {
             this.card = document.querySelector(".card")
@@ -10,15 +11,13 @@
             this.cardUserName = document.querySelector('*[name="card-user-name"]');
             this.cardBgFlag = document.querySelector(".card-bg-flag");
             this.cardBgWrap = document.querySelector(".card-bg-wrapper");
-            this.cardBg = document.querySelector(".card-bg-1")
-            this.cardBg2 = document.querySelector(".card-bg-2")
-            this.cardBg3 = document.querySelector(".card-bg-3")
-            this.cardBg4 = document.querySelector(".card-bg-4")
-            this.cardBg5 = document.querySelector(".card-bg-5")
-
+            this.cardClassLength = 1
+            this.cardBgs = [];
+            document.querySelectorAll(".card-bg").forEach(e=> this.cardBgs.push(e))
         },
         bindEvent: function () {
             var self = this;
+
             self.cardName.addEventListener('input', function () {
                 self.cardUserName.innerHTML = self.cardName.value
 
@@ -26,21 +25,20 @@
             self.cardBgFlag.addEventListener('click',function(){
                 self.cardBgWrap.classList.add('card-bg-wrapper_new-position')
             }) 
-            self.cardBg.addEventListener('click', function(e){
-                self.card.classList.add("card-bg-1")
-            }) 
-            self.cardBg2.addEventListener('click', function(e){
-                self.card.classList.add("card-bg-2")
-            }) 
-            self.cardBg3.addEventListener('click', function(e){
-                self.card.classList.add("card-bg-3")
-            }) 
-            self.cardBg4.addEventListener('click', function(e){
-                self.card.classList.add("card-bg-4")
-            }) 
-            self.cardBg5.addEventListener('click', function(e){
-                self.card.classList.add("card-bg-5")
-            })  
+            self.cardBgs.forEach(function(e){
+                e.addEventListener('click', function(e){
+                    [...self.card.classList].filter(function(e){
+                     return e.startsWith('card-bg')
+                    }).forEach(function(e){
+                        self.card.classList.remove(e)
+
+                    })
+                   var target = e.target 
+                   var targetBg = target.classList[target.classList.length -1]
+                   self.card.classList.add(targetBg)
+                })
+            })
+           
         }
     };
     card.init()
